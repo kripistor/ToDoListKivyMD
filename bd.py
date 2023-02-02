10d98efa6e428d78a948a71c317a3b861a045e54
kripistor <liss01062004l@gmail.com>
2023-02-02 13:08:27 +0300

    bd add

--- bd.py
+++ bd.py
@@ -1,6 +1,11 @@
+from sqlalchemy import Table, Column, String, MetaData, Boolean
 from sqlalchemy import create_engine
-engine = create_engine("mysql://user:pwd@localhost/college",echo = True)
-from sqlalchemy import Table, Column, Integer, String, MetaData
-meta = MetaData()
 
+engine = create_engine('sqlite:///tasks.db', echo=True)
+meta = MetaData()
 tasks = Table(
+    'tasks', meta,
+    Column('task', String, primary_key=True),
+    Column('is_done', Boolean, default=False),
+)
+meta.create_all(engine)
--- main.py
+++ main.py
@@ -1,25 +1,40 @@
-import time
-
 from kivymd.app import MDApp
 from kivymd.uix.list import OneLineAvatarIconListItem, \
     ILeftBodyTouch
 from kivymd.uix.pickers import MDDatePicker
 from kivymd.uix.selectioncontrol import MDCheckbox
+from sqlalchemy import Table, Column, Integer, String, MetaData
 
+from bd import meta, tasks, engine
 
 class ListItemWithCheckbox(OneLineAvatarIconListItem):
+    def __init__(self, pk=None, **kwargs):
+        super().__init__(**kwargs)
+        # state a pk which we shall use link the list items with the database primary keys
+        self.pk = pk
     def mark(self, check, the_list_item):
         if check.active == True:
             # add strikethrough to the text if the checkbox is active
             the_list_item.text = '[s]' + the_list_item.text + '[/s]'
+            text = the_list_item.text.replace('[s]', '')
+            text = text.replace('[/s]', '')
+            conn = engine.connect()
+            conn.execute(tasks.update().where(tasks.c.task == text).values(is_done=True))
+            conn.commit()
+
         else:
             the_list_item.text = str(the_list_item.text.replace('[s]', ''))
-            pass
+            the_list_item.text = str(the_list_item.text.replace('[/s]', ''))
+            conn = engine.connect()
+            conn.execute(tasks.update().where(tasks.c.task == the_list_item.text).values(is_done=False))
+            conn.commit()
 
     def delete_item(self, the_list_item):
 
         self.parent.remove_widget(the_list_item)
-
+        conn = engine.connect()
+        conn.execute(tasks.delete().where(tasks.c.task == the_list_item.text))
+        conn.commit()
 
 class LeftCheckbox(ILeftBodyTouch, MDCheckbox):
     '''Custom right container.'''
@@ -28,21 +43,35 @@ class LeftCheckbox(ILeftBodyTouch, MDCheckbox):
 class MainApp(MDApp):
     def build(self):
         self.theme_cls.theme_style = "Dark"
+    def on_start(self):
+        conn = engine.connect()
+        completed = conn.execute(tasks.select().where(tasks.c.is_done == True))
+        un_completed = conn.execute(tasks.select().where(tasks.c.is_done == False))
+        for row in completed:
+            self.root.ids.list.add_widget(ListItemWithCheckbox(pk=True,text = '[s]' + row[0] + '[/s]'))
+        for row in un_completed:
+            self.root.ids.list.add_widget(ListItemWithCheckbox(pk=False,text=row[0]))
+
 
     def add_item(self, text):
-        self.root.ids.list.add_widget(ListItemWithCheckbox(text=text + time.strftime("%H:%M:%S", time.localtime())))
+        self.root.ids.list.add_widget(ListItemWithCheckbox(text=text))
         self.root.ids.listinput.text = ""
+        ins = tasks.insert().values(task=text)
+        conn = engine.connect()
+        result = conn.execute(ins)
+        conn.commit()
 
-    def delete_item(self, the_list_item):
-        self.root.remove_widget(the_list_item)
-
-    def on_save(self, instance, value, date_range):
-        print(instance, value, date_range)
+    def on_save(self,instance,*args,**kwargs):
+        print(args)
+        print(kwargs)
+        #self.root.ids.list.add_widget(ListItemWithCheckbox(text=str(value)))
+        #self.root.ids.listinput.text = ""
 
     def on_cancel(self, instance, value):
         '''Events called when the "CANCEL" dialog box button is clicked.'''
 
-    def show_date_picker(self):
+    def show_date_picker(self, text):
+        print(text)
         date_dialog = MDDatePicker()
         date_dialog.bind(on_save=self.on_save, on_cancel=self.on_cancel)
         date_dialog.open()
